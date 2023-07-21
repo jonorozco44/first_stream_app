647177f9b6f4ca1e3527a0c2f6f4e01e8895813b
jonorozco44 <140159499+jonorozco44@users.noreply.github.com>
2023-07-21 11:20:51 +0100

    Update streamlit_app.py
    test5

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title("My Parents New Healthy Dinner")
 
 streamlit.header("Menu")
+
+streamlit.text("Blueberry Oatmeal")
+streamlit.text("Porridge")
+streamlit.text("Boiled Eggs")
